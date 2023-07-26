0929c7ca65b279c1af7260b47b57171cfe82ac83
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-07-26 15:44:10 +0200

    Update git tracking with .gitignore

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Node_1 = __importDefault(require("./Node"));
-let root = new Node_1.default(5);
-console.log(root.value);
