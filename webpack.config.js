47fd703317c4815c370b858937642bcca5285885
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-07-27 14:34:50 +0200

    Add jest configuration

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       }
     ]
   },
@@ -27,5 +31,5 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
 }
