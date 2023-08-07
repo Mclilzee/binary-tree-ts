1e7079445ca60f4865a82e26154884bd9a5b0789
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-07 22:24:17 +0200

    Adjust styling to display controls

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,11 @@ import "./style.css";
 const tree = new BinaryTree();
 const numbers = getRandomNumbers();
 tree.buildNewTree(numbers);
-console.log(numbers);
 
 function getRandomNumbers(): number[] {
   const numbers: number[] = []
   for (let i = 0; i < 1000; i++) {
-    const number = Math.floor(Math.random() * 20);
-    if (numbers.includes(number)) {
-      continue;
-    }
-
+    const number = Math.floor(Math.random() * 100);
     numbers.push(number);
   }
 
