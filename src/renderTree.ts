2f92e973bf9374b86e26d46e57ce65e5b6779294
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-09 06:45:14 +0200

    Draw lines

--- src/renderTree.ts
+++ src/renderTree.ts
@@ -14,6 +14,7 @@ function renderTree(tree: BinaryTree) {
   }
 
   drawElemenets(binaryTreeContainer, tree.root);
+  drawLines();
 }
 
 
@@ -38,4 +39,56 @@ function drawElemenets(container: HTMLDivElement, node: TNode) {
   }
 }
 
+function drawLines() {
+  const values = document.querySelectorAll(".value");
+  values.forEach(value => {
+    let nextSibling = value.nextElementSibling;
+
+    if (nextSibling !== null) {
+      const siblingValue = nextSibling.firstElementChild as HTMLDivElement;
+      drawLine(value as HTMLDivElement, siblingValue);
+      nextSibling = nextSibling.nextElementSibling;
+    }
+
+    if (nextSibling !== null) {
+      const siblingValue = nextSibling.firstElementChild as HTMLDivElement;
+      drawLine(value as HTMLDivElement, siblingValue);
+      nextSibling = nextSibling.nextElementSibling;
+    }
+  });
+}
+
+function drawLine(start: HTMLDivElement, end: HTMLDivElement) {
+  const thickness = 2;
+  const line = document.createElement("div");
+  line.classList.add("line");
+
+  const e1 = getOffset(start);
+  const e2 = getOffset(end);
+  const length = Math.sqrt(((e2.x - e1.x) * (e2.x - e1.x)) + ((e2.y - e1.y) * (e2.y - e1.y)));
+
+  const cx = ((e1.x + e2.x) / 2) - (length / 2);
+  const cy = ((e1.y + e2.y) / 2) - (thickness / 2);
+  // angle
+  const angle = Math.atan2((e1.y - e2.y), (e1.x - e2.x)) * (180 / Math.PI);
+  // make hr
+
+  line.style.position = "absolute";
+  line.style.left = cx + "px";
+  line.style.top = cy + "px";
+  line.style.width = length + "px";
+  line.style.height = thickness + "px";
+  line.style.rotate = angle + "deg";
+
+  binaryTreeContainer.appendChild(line);
+}
+
+function getOffset(element: HTMLDivElement) {
+  const rect = element.getBoundingClientRect();
+
+  return {
+    x: rect.left + rect.width,
+    y: rect.top + rect.height
+  }
+}
 export default renderTree;
