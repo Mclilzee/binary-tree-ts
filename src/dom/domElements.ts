3dd5e778165cb95640b6bb384ab536e117c0d21f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-14 12:58:37 +0200

    Add tree creation event listener

--- src/dom/domElements.ts
+++ src/dom/domElements.ts
@@ -1,5 +1,6 @@
 const binaryTreeContainer = document.querySelector(".binary-tree-container") as HTMLDivElement;
 const removeButton = document.querySelector(".remove-button") as HTMLDivElement;
 const addValueForm = document.querySelector("form.add-new-value") as HTMLDivElement;
+const createTreeForm = document.querySelector("form.create-tree") as HTMLFormElement;
 
-export { binaryTreeContainer, removeButton, addValueForm }
+export { binaryTreeContainer, removeButton, addValueForm, createTreeForm }
---
+++ src/dom/listeners/createTreeListener.ts
@@ -0,0 +1,20 @@
+import { createTreeForm } from "../domElements";
+import tree from "../../tree";
+import renderTree from "../renderTree";
+
+function createTreeListener() {
+  createTreeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const textArray = form.querySelector("input[name=text-array]") as HTMLInputElement;
+    const values = textArray.value.split(" ")
+      .map(value => parseInt(value))
+      .filter(value => value >= 0 && value <= 99);
+
+    tree.buildNewTree(values);
+    renderTree(tree);
+  })
+}
+
+export default createTreeListener;
