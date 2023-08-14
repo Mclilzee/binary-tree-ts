dd6ce50042016921e85198fa8e1cea1651616c75
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-14 13:05:04 +0200

    Attach listener

--- src/dom/eventHandling.ts
+++ src/dom/eventHandling.ts
@@ -1,10 +1,12 @@
 import addValueListner from "./listeners/addValueListener";
+import createTreeListener from "./listeners/createTreeListener";
 import removeValueListener from "./listeners/removeValueListener";
 import selectValueListener from "./listeners/selectValueListener";
 function attachEventListeners() {
   selectValueListener();
   removeValueListener();
   addValueListner();
+  createTreeListener();
 }
 
 export { attachEventListeners };
