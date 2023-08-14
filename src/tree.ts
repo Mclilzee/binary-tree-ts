5d1a9729d9634be2cdd14e841f44ed8dd6aa3648
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-14 15:11:28 +0200

    Fix initial tree

--- src/tree.ts
+++ src/tree.ts
@@ -1,6 +1,6 @@
 import BinaryTree from "./BinaryTree";
 
 const tree = new BinaryTree();
-tree.buildNewTree([100, 200, 300, 400, 500, 450, 600, 50, 60, 70, 80, 90, 65, 64, 40, 30, 20, 25]);
+tree.buildNewTree([100, 200, 300, 400, 500, 450, 600, 50, 60, 70, 80, 90, 65, 64, 40, 30, 20, 25, 35, 45, 43, 42, 46, 55, 53, 180, 150, 170, 160, 190, 195, 140, 130, 250, 240]);
 
 export default tree;
