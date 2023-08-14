4adfdfed754da7caa55de578bc7499f9b6be5bbf
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-14 15:12:40 +0200

    Fix test for complex binary tree

--- src/test/BinaryTree/traversing.test.ts
+++ src/test/BinaryTree/traversing.test.ts
@@ -219,7 +219,7 @@ describe("Check if tree is balance", () => {
 
   test("Tree balanced for complex tree structure", () => {
     const tree = new BinaryTree();
-    tree.buildNewTree([100, 200, 300, 400, 500, 450, 600, 50, 60, 70, 80, 90, 65, 64, 40, 30, 20, 25]);
+    tree.buildNewTree([100, 200, 300, 400, 500, 450, 600, 50, 60, 70, 80, 90, 65, 64, 40, 30, 20, 25, 35, 45, 43, 42, 46, 55, 53, 180, 150, 170, 160, 190, 195, 140, 130, 250, 240]);
     expect(tree.isBalanced()).toBe(true);
   })
 })
